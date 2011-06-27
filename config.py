4c3e331f4c0b60fc9ff164af1e8761784966c489
darktable <calvin@darktable.com>
2011-06-27 11:14:25 -0700

    switch to allow all admin users to see profiler tab.

--- config.py
+++ config.py
@@ -7,8 +7,14 @@ enabled_profiler_emails = [
     "test1@example.com",
 ]
 
+enable_profiler_admins = True
+
 # Customize should_profile to return true whenever a request should be profiled.
 # This function will be run once per request, so make sure its contents are fast.
 def should_profile(environ):
     user = users.get_current_user()
+
+    if enable_profiler_admins and users.is_current_user_admin():
+        return True
+
     return user and user.email() in enabled_profiler_emails
