e96915431069e5c38736a2f5d8e6b45e64a940c9
Ben Kamens <kamens@gmail.com>
2011-06-29 23:44:22 -0700

    Slight whitespace change/code reformat

--- config.py
+++ config.py
@@ -12,13 +12,14 @@ enable_profiler_admins = True
 # Customize should_profile to return true whenever a request should be profiled.
 # This function will be run once per request, so make sure its contents are fast.
 def should_profile(environ):
-    user = users.get_current_user()
 
-	# Never profile calls to the profiler itself to avoid endless recursion.
-	if environ["PATH_INFO"].startswith("/gae_mini_profiler/"):
-		return False
+    # Never profile calls to the profiler itself to avoid endless recursion.
+    if environ["PATH_INFO"].startswith("/gae_mini_profiler/"):
+        return False
 
     if enable_profiler_admins and users.is_current_user_admin():
         return True
+
+    user = users.get_current_user()
 
     return user and user.email() in enabled_profiler_emails
